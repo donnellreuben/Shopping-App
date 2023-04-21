62836ca63f303be8958ae0a6c138b6c79204b8c1
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-21 12:44:06 -0500

    Added routes to all users to add and remove products from their cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :cart, only: [:show] do
+    post 'add_to_cart/:product_id', action: :add_to_cart, as: 'add_to_cart'
+    delete 'remove_from_cart/:id', action: :remove_from_cart, as: 'remove_from_cart'
+  end
+  resources :orders
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  root to: 'products#index'
 end

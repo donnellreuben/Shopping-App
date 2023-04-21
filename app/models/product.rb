bedd296d51b38a7694f605d5251707a2a6eec027
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-21 12:41:10 -0500

    Added Product Validations and Associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
+# VALIDATIONS
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+# ASSOCIATIONS
+  has_many :cart_items
+  has_many :order_items
+  has_many :carts, through: :cart_items
 end

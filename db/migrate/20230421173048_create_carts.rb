c73d0f13bf1ae532a875c6d49ab01fc347f43196
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-21 12:32:11 -0500

    deleted Cart model, then dropped database, then recreated it with a total attribute and re created databased and migrated it

--- db/migrate/20230421173048_create_carts.rb
+++ db/migrate/20230421173048_create_carts.rb
@@ -1,6 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
+      t.decimal :total
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_172712) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_173048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
+    t.decimal "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
